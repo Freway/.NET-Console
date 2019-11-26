361a49f7969728c8128799fbac58768e0c8885de
Freway <gui-h-m@outlook.com>
2019-11-26 17:46:18 -0300

    Error Log
    Added log

--- Reverse.Geocode/Reverse.Geocode/Program.cs
+++ Reverse.Geocode/Reverse.Geocode/Program.cs
@@ -8,9 +8,7 @@ using System.Net;
 using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 
-
 //By Guilherme Hernandes Monteiro
-
 
 namespace Reverse.Geocode
 {
@@ -28,53 +26,55 @@ namespace Reverse.Geocode
 
             using (StreamReader sr = new StreamReader(path + "/end.txt"))
             {
-                string line;
-                int count = 0;
-
-                //Cria uma planilha temporária na memória do computador
-                xlApp = new Excel.Application();
-                xlWorkBook = xlApp.Workbooks.Add(misValue);
-                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-                while ((line = sr.ReadLine()) != null)
+                using (StreamWriter error = File.CreateText(path + "/erros.txt"))
                 {
-                    string address = line;
-                    string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&address={0}&sensor=false", Uri.EscapeDataString(address), "AIzaSyCccJxCtZhZrYIKjh6pDEzfIrm9e3fj0bk");
-                    count++;
-                    string lat1, lng1;
+                    string line;
+                    int count = 0;
 
-                    try
+                    //Cria uma planilha temporária na memória do computador
+                    xlApp = new Excel.Application();
+                    xlWorkBook = xlApp.Workbooks.Add(misValue);
+                    xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                    while ((line = sr.ReadLine()) != null)
                     {
-                        WebRequest request = WebRequest.Create(requestUri);
-                        WebResponse response = request.GetResponse();
-                        XDocument xdoc = XDocument.Load(response.GetResponseStream());
+                        string address = line;
+                        string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&address={0}&sensor=false", Uri.EscapeDataString(address), "AIzaSyCccJxCtZhZrYIKjh6pDEzfIrm9e3fj0bk");
+                        count++;
+                        string lat1, lng1;
 
-                        //incluindo dados  
-                        xlWorkSheet.Cells[count, 1] = "ofn" + count;
-                        XElement result = xdoc.Element("GeocodeResponse").Element("result");
-                        XElement locationElement = result.Element("geometry").Element("location");
-                        XElement lat = locationElement.Element("lat");
-                        lat1 = lat.ToString();
-                        lat1 = lat1.Substring(5, 8);
-                        xlWorkSheet.Cells[count, 2] = lat1;
-                        XElement lng = locationElement.Element("lng");
-                        lng1 = lng.ToString();
-                        lng1 = lng1.Substring(5, 8);
-                        xlWorkSheet.Cells[count, 3] = lng1;
+                        try
+                        {
+                            WebRequest request = WebRequest.Create(requestUri);
+                            WebResponse response = request.GetResponse();
+                            XDocument xdoc = XDocument.Load(response.GetResponseStream());
 
-                        Console.WriteLine(count + " " + line);
+                            //incluindo dados  
+                            xlWorkSheet.Cells[count, 1] = "ofn" + count;
+                            XElement result = xdoc.Element("GeocodeResponse").Element("result");
+                            XElement locationElement = result.Element("geometry").Element("location");
+                            XElement lat = locationElement.Element("lat");
+                            lat1 = lat.ToString();
+                            lat1 = lat1.Substring(5, 8);
+                            xlWorkSheet.Cells[count, 2] = lat1;
+                            XElement lng = locationElement.Element("lng");
+                            lng1 = lng.ToString();
+                            lng1 = lng1.Substring(5, 8);
+                            xlWorkSheet.Cells[count, 3] = lng1;
+
+                            Console.WriteLine(count + " " + line);
+                        }
+                        catch
+                        {
+                            error.WriteLine("Erro linha: " + count + " (" + line + ")");
+                        }
                     }
-                    catch
-                    {
-
-
-                    }
+                    //Salva o arquivo de acordo com a documentação do Excel.
+                    xlWorkBook.SaveAs(path + "/arquivo.csv", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
+                    Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                    xlWorkBook.Close(true, misValue, misValue);
+                    xlApp.Quit();
                 }
-                //Salva o arquivo de acordo com a documentação do Excel.
-                xlWorkBook.SaveAs(path + "/arquivo.csv", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
-                Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-                xlWorkBook.Close(true, misValue, misValue);
-                xlApp.Quit();
             }
         }
     }
