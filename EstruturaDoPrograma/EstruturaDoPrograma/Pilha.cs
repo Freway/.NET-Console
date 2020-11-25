43f272f3d24c6191119ca664e145e533e6c9c533
Guilherme Hernandes Monteiro <gui-h-m@outlook.com>
2020-11-25 13:59:13 -0300

    First Commit
    First Commit EstruturaDoPrograma

---
+++ EstruturaDoPrograma/EstruturaDoPrograma/Pilha.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstruturaDoPrograma
+{
+    public class Pilha
+    {
+        Posicao primeiro;
+
+        public void Empilha(object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+
+        public object Desempilha() 
+        {
+            if(primeiro == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;
+        }
+    }
+}
---
+++ EstruturaDoPrograma/EstruturaDoPrograma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String entrada = null;
+            var s = new Pilha();
+
+            Console.WriteLine("Digite tres valores para inserir na pilha:");
+            for (int i = 0; i < 3; i++)
+            {
+                s.Empilha(entrada = Console.ReadLine());
+            }
+
+            for (int i=0;i < 3;i++) 
+            {
+                Console.WriteLine(s.Desempilha()); 
+            }
+        }
+    }
+}
